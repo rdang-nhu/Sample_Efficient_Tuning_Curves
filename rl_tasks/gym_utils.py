7e99698c92997ea1ee3a9f5861cf5c9dd815295d
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2019-08-01 17:26:58 +0200

    Cleaning repo

--- rl_tasks/gym_utils.py
+++ rl_tasks/gym_utils.py
@@ -2,7 +2,6 @@
 # Author: Raphael Dang-Nhu
 # Date: 18/05/2019
 
-import deep_policy
 import bump_policy
 
 def get_policy(env_name,algo_config,c_conf,debug_file,perf_file):
@@ -10,5 +9,5 @@ def get_policy(env_name,algo_config,c_conf,debug_file,perf_file):
     algo_name = algo_config["name"]
     if(algo_name == "bump"):
         return bump_policy.BumpPolicy(env_name,algo_config,c_conf,debug_file,perf_file)
-    elif(algo_name == "deep"):
-        return deep_policy.DeepPolicy(env_name,algo_config)
+    else: 
+        raise ValueError("Algorithm does not exist")
